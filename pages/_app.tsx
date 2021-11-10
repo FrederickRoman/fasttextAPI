65a5eb303796d450217dbdc28b7c20090d986b8d
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2021-11-09 23:22:04 -0800

    feat(_app): Add theme and title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,34 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import * as React from "react";
+import PropTypes from "prop-types";
+import Head from "next/head";
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
+import theme from '@/themes/ftaTheme';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const cache = createCache({ key: "css" });
+cache.compat = true;
+
+interface props {
+  Component: PropTypes.ReactComponentLike;
+  pageProps: object;
 }
-export default MyApp
+
+export default function MyApp(props: props) {
+  const { Component, pageProps } = props;
+  return (
+    <CacheProvider value={cache}>
+      <Head>
+        <title>fastText API</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        {/* <MainBar /> */}
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
+}
