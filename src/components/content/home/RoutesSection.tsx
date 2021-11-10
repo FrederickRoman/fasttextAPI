eabe04b0a9a74fcdeb536cc0ab18224af4e56b92
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2021-11-09 23:16:15 -0800

    feat(RoutesSection): Add section that describe the main API entrypoints

---
+++ src/components/content/home/RoutesSection.tsx
@@ -0,0 +1,54 @@
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+
+function RoutesHeader(): JSX.Element {
+  return <h2>Routes</h2>;
+}
+function PostRoute(): JSX.Element {
+  return (
+    <Grid container justifyContent="center" alignItems="center">
+      <Grid item>
+        <Box mx={1}>
+          <span>POST /api/word/</span>
+        </Box>
+      </Grid>
+      <Grid item>
+        <Box mx={1}>
+          <pre>body:&#123;words:['list', 'of', 'words']&#125; </pre>
+        </Box>
+      </Grid>
+    </Grid>
+  );
+}
+
+function RoutesDescription(): JSX.Element {
+  return (
+    <Grid container justifyContent="center" alignItems="center">
+      <Grid item>
+        <PostRoute />
+      </Grid>
+    </Grid>
+  );
+}
+
+function RoutesSection(): JSX.Element {
+  return (
+    <section>
+      <Grid
+        container
+        direction="column"
+        justifyContent="space-evenly"
+        alignItems="center"
+      >
+        <Grid item>
+          <RoutesHeader />
+        </Grid>
+        <Grid item>
+          <RoutesDescription />
+        </Grid>
+      </Grid>
+    </section>
+  );
+}
+
+export default RoutesSection;
