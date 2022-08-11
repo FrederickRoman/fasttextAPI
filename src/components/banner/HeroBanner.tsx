92ef7bd59486922f277209debc28074d735491f5
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2022-08-11 00:16:28 -0700

    fix(components): Remove useStyles to avoid rendering errors

--- src/components/banner/HeroBanner.tsx
+++ src/components/banner/HeroBanner.tsx
@@ -3,49 +3,70 @@ import heroBgPic from "../../../public/img/hero_bg.jpeg";
 
 import { Theme } from "@mui/material/styles";
 
-import createStyles from '@mui/styles/createStyles';
-import makeStyles from '@mui/styles/makeStyles';
+import createStyles from "@mui/styles/createStyles";
+import makeStyles from "@mui/styles/makeStyles";
+import { Box } from "@mui/material";
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    section_container_layerStack: {
-      display: "grid",
-      justifyItems: "start",
-      alignItems: "start",
-      "& > *": {
-        gridColumnStart: 1,
-        gridRowStart: 1,
-      },
-    },
-    header_container_center: {
-      width: "100%",
-      height: "100%",
-      zIndex: 1,
-      display: "grid",
-      justifyContent: "center",
-      alignContent: "center",
-    },
-    textMsg: {
-      color: theme.palette.background.default,
-      fontSize: "clamp(2rem, 10vw, 5rem)",
-      background: "rgba(87, 12, 29, 0.2)",
-    },
-  })
-);
+// const useStyles = makeStyles((theme: Theme) =>
+//   createStyles({
+//     section_container_layerStack: {
+//       display: "grid",
+//       justifyItems: "start",
+//       alignItems: "start",
+//       "& > *": {
+//         gridColumnStart: 1,
+//         gridRowStart: 1,
+//       },
+//     },
+//     // header_container_center: {
+//     //   width: "100%",
+//     //   height: "100%",
+//     //   zIndex: 1,
+//     //   display: "grid",
+//     //   justifyContent: "center",
+//     //   alignContent: "center",
+//     // },
+//     // textMsg: {
+//     //   color: theme.palette.background.default,
+//     //   fontSize: "clamp(2rem, 10vw, 5rem)",
+//     //   background: "rgba(87, 12, 29, 0.2)",
+//     // },
+//   })
+// );
 
 function HeroBanner(): JSX.Element {
-  const classes = useStyles();
   return (
-    <section role="banner" className={classes.section_container_layerStack}>
+    <Box component="section" role="banner" position="relative">
       <Image
         src={heroBgPic}
         priority
         alt="background: yarn-like network graph"
       />
-      <header className={classes.header_container_center}>
-        <span className={classes.textMsg}> fastText API</span>
-      </header>
-    </section>
+      <Box position="absolute" top="50%" left="50%">
+        <header
+          style={{
+            width: "100%",
+            height: "100%",
+            zIndex: 1,
+            display: "grid",
+            justifyContent: "center",
+            alignContent: "center",
+            transform: "translate(-50%, -50%)",
+          }}
+        >
+          <Box
+            sx={{
+              color: "background.default",
+              fontSize: "clamp(2rem, 10vw, 5rem)",
+              background: "rgba(87, 12, 29, 0.2)",
+              textAlign: "center",
+            }}
+          >
+            fastText API
+          </Box>
+        </header>
+      </Box>
+    </Box>
   );
 }
 
--- src/components/content/home/IntroSection.tsx
+++ src/components/content/home/IntroSection.tsx
@@ -1,20 +1,7 @@
-import createStyles from '@mui/styles/createStyles';
-import makeStyles from '@mui/styles/makeStyles';
-
-const useStyles = makeStyles(() =>
-  createStyles({
-    intro_p: {
-      fontSize: "clamp(2em, 5vw, 5em)",
-      textAlign: "center"
-    },
-  })
-);
-
 function IntroSection(): JSX.Element {
-  const classes = useStyles();
   return (
     <section>
-      <p className={classes.intro_p}>
+      <p style={{ fontSize: "clamp(2em, 5vw, 5em)", textAlign: "center" }}>
         Fastext API is an unnoficial API for Facebook's fastText word embedding
         used under a public license
       </p>
