95ae2420e6227aecd8b724addeeb2b1def174f9a
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2022-08-12 18:54:07 -0700

    refactor(HeroBanner): Remove unneeded comments

--- src/components/banner/HeroBanner.tsx
+++ src/components/banner/HeroBanner.tsx
@@ -1,38 +1,6 @@
 import Image from "next/image";
 import heroBgPic from "../../../public/img/hero_bg.jpeg";
-
-import { Theme } from "@mui/material/styles";
-
-import createStyles from "@mui/styles/createStyles";
-import makeStyles from "@mui/styles/makeStyles";
 import { Box } from "@mui/material";
-
-// const useStyles = makeStyles((theme: Theme) =>
-//   createStyles({
-//     section_container_layerStack: {
-//       display: "grid",
-//       justifyItems: "start",
-//       alignItems: "start",
-//       "& > *": {
-//         gridColumnStart: 1,
-//         gridRowStart: 1,
-//       },
-//     },
-//     // header_container_center: {
-//     //   width: "100%",
-//     //   height: "100%",
-//     //   zIndex: 1,
-//     //   display: "grid",
-//     //   justifyContent: "center",
-//     //   alignContent: "center",
-//     // },
-//     // textMsg: {
-//     //   color: theme.palette.background.default,
-//     //   fontSize: "clamp(2rem, 10vw, 5rem)",
-//     //   background: "rgba(87, 12, 29, 0.2)",
-//     // },
-//   })
-// );
 
 function HeroBanner(): JSX.Element {
   return (
