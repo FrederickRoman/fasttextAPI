1546a914b1cfdb0845c0a6e3b77e04c620b89658
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2022-10-03 16:31:57 -0700

    feat(_app): Add icon Head links

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,15 @@
 import * as React from "react";
 import PropTypes from "prop-types";
 import Head from "next/head";
-import { ThemeProvider, Theme, StyledEngineProvider } from "@mui/material/styles";
+import {
+  ThemeProvider,
+  Theme,
+  StyledEngineProvider,
+} from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import { CacheProvider } from "@emotion/react";
 import createCache from "@emotion/cache";
-import theme from '@/themes/ftaTheme';
-
-
-declare module '@mui/styles/defaultTheme' {
-  interface DefaultTheme extends Theme {}
-}
-
+import theme from "@/themes/ftaTheme";
 
 const cache = createCache({
   key: "css",
@@ -31,6 +29,24 @@ export default function MyApp(props: props) {
       <Head>
         <title>fastText API</title>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        ></link>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        ></link>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        ></link>
+        <link rel="manifest" href="/site.webmanifest"></link>
       </Head>
       <StyledEngineProvider injectFirst>
         <ThemeProvider theme={theme}>
