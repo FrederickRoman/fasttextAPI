3e57ed58720c55b1c19ad0eca4e25387b27d230f
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2022-10-03 01:19:52 -0700

    feat(IntroSection): Show section as dark mode

--- src/components/content/home/IntroSection.tsx
+++ src/components/content/home/IntroSection.tsx
@@ -1,11 +1,17 @@
+import { Box } from "@mui/material";
+
 function IntroSection(): JSX.Element {
   return (
-    <section>
+    <Box
+      component="section"
+      px={5}
+      sx={{ bgcolor: "black", color: "lightgray", borderRadius: 5 }}
+    >
       <p style={{ fontSize: "clamp(2em, 5vw, 5em)", textAlign: "center" }}>
         Fastext API is an unnoficial API for Facebook's fastText word embedding
         used under a public license
       </p>
-    </section>
+    </Box>
   );
 }
 
