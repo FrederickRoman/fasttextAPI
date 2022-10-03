280fae93080309a346c9db99dd59f81da819e9bb
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2022-10-03 01:44:53 -0700

    feat(DemoSection): Fetch heatmap data for words

--- src/components/demo/DemoSection.tsx
+++ src/components/demo/DemoSection.tsx
@@ -1,31 +1,57 @@
+import { useState, useEffect } from "react";
 import { Box, Grid, Typography } from "@mui/material";
-import { useEffect, useState } from "react";
 import DemoWordAdd from "./input/DemoWordAdd";
 import DemoHeatmap from "./output/DemoHeatmap";
+import ApiCallService from "@/services/client/ApiCallService";
+import DataAnalysisService from "@/services/client/DataAnalysisService";
 
-const DEFAULT_WORDS: string[] = ["cat", "dog", "pet", "human", "animal"];
-const DEFAULT_DATA: number[][] = new Array(DEFAULT_WORDS.length)
-  .fill(0)
-  .map(() =>
-    new Array(DEFAULT_WORDS.length)
-      .fill(0)
-      .map(() => Math.floor(Math.random() * 50 + 50))
-  );
+const DEFAULT_WORDS: Readonly<string[]> = Object.freeze([
+  "cat",
+  "dog",
+  "pet",
+  "human",
+  "animal",
+]);
+const DEFAULT_DATA: Readonly<number[][]> = Object.freeze([
+  [1, 0.24, 0.45, 0.23, 0.4],
+  [0.24, 1, 0.25, 0.3, 0.23],
+  [0.45, 0.25, 1, 0.19, 0.35],
+  [0.23, 0.3, 0.19, 1, 0.41],
+  [0.4, 0.23, 0.35, 0.41, 1],
+]);
 
 function DemoSection(): JSX.Element {
-  const [words, setWords] = useState<string[]>(DEFAULT_WORDS);
-  const [data, setData] = useState<number[][]>(DEFAULT_DATA);
+  const [words, setWords] = useState<string[]>([...DEFAULT_WORDS]);
+  const [data, setData] = useState<number[][]>([...DEFAULT_DATA]);
 
   useEffect(() => {
-    setData(
-      new Array(words.length)
-        .fill(0)
-        .map(() =>
-          new Array(words.length)
-            .fill(0)
-            .map(() => Math.floor(Math.random() * 50 + 50))
-        )
-    );
+    let isActive: boolean = true;
+    const round = (decimal: number): number => Math.round(100 * decimal) / 100;
+
+    async function fetchHeatmapData(words: string[]): Promise<void> {
+      try {
+        if (words.length) {
+          const embeddings = await ApiCallService.getFastTextEmbeddings(words);
+          console.log(embeddings);
+          if (isActive) {
+            const coseSimMat = DataAnalysisService.getHeatMap(embeddings);
+            const roundedCosSimMat = coseSimMat.map((row) => row.map(round));
+            setData(roundedCosSimMat);
+          }
+        } else {
+          if (isActive) setData([]);
+        }
+      } catch (error) {
+        console.log(error);
+        setData([]);
+      }
+    }
+
+    fetchHeatmapData(words);
+    return () => {
+      isActive = false;
+      setData([]);
+    };
   }, [words.toString()]);
 
   return (
@@ -42,6 +68,8 @@ function DemoSection(): JSX.Element {
             <Grid item>
               <Box m={6}>
                 <Typography variant="h4">Compare words</Typography>
+                <Typography variant="h4">0 = different</Typography>
+                <Typography variant="h4">1 = similar</Typography>
               </Box>
             </Grid>
             <Grid item>
@@ -50,7 +78,9 @@ function DemoSection(): JSX.Element {
           </Grid>
         </Grid>
         <Grid item width="100%">
-          <DemoHeatmap xLabels={words} yLabels={words} data={data} />
+          {words.length && (
+            <DemoHeatmap xLabels={words} yLabels={words} data={data} />
+          )}
         </Grid>
       </Grid>
     </Box>
