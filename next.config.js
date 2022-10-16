309ce3108caddd68ab9383e3d113c2bfab2bd385
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2022-10-15 22:35:01 -0700

    build(next.config): Config withPWA

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  scope: "/",
+  disable: process.env.NODE_ENV === "development",
+  sw: "sw.js",
+});
+
 /** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfig = withPWA({
   reactStrictMode: true,
-}
+  swcMinify: true,
+});
+
+module.exports = nextConfig;
