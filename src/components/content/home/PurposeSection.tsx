0e2dc9e468e4df7b7202d05011a20a3de8bc3d1f
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2022-10-16 13:27:01 -0700

    feat(PurposeSection): Add purpose description

---
+++ src/components/content/home/PurposeSection.tsx
@@ -0,0 +1,31 @@
+import { Box, Stack, Typography } from "@mui/material";
+
+function PurposeSection(): JSX.Element {
+  return (
+    <Box component="section" px={2}>
+      <Stack spacing={2}>
+        <Typography variant="h2" style={{textAlign: "center"}}>Purpose</Typography>
+        <Stack spacing={2}>
+          <p>
+            Word embedding enable the use of state of the art NLP DL models.
+          </p>
+          <p>
+            However, you need those embeddings on inference (not just on
+            training).
+          </p>
+          <p>
+            If you decide to run the inference client-side, you either have to
+            bundle the embeddings with client code (too impractical) or use a
+            server API.
+          </p>
+          <p>
+            With this API service, you get word embeddings to run state of the
+            art deep learning NLP inference client-side.
+          </p>
+        </Stack>
+      </Stack>
+    </Box>
+  );
+}
+
+export default PurposeSection;
